82c8bb57f5dd7ea6e5ea9f2695b223a98d17ad17
Kyle Marshall <kylemarshall88@gmail.com>
2017-06-04 17:36:06 -0700

    updating model

--- train.py
+++ train.py
@@ -16,12 +16,13 @@ VALID_STEP = 10
 CELL_SIZE = 5
 CELL_TYPE = 'gru' # gru or lstm
 NUM_LAYERS = 3
+BATCH_SIZE = 16
 
 dataset = DataSet(FLAGS.dataset)
 
 def main(*args):
 	
-	model = Model(CELL_SIZE, NUM_LAYERS)
+	model = Model(CELL_SIZE, NUM_LAYERS, BATCH_SIZE)
 
 	with tf.Session() as sess:
 
@@ -30,10 +31,10 @@ def main(*args):
 		for step in range(TRAINING_STEPS):
 
 			if step % VALID_STEP == 0:
-				samples_batch, labels_batch = dataset.get_batch(10,'valid')
+				samples_batch, labels_batch = dataset.get_batch(BATCH_SIZE, 'valid')
 				model.step(samples_batch, labels_batch, 'valid')
 			else:
-				samples_batch, labels_batch = dataset.get_batch(10,'train')
+				samples_batch, labels_batch = dataset.get_batch(BATCH_SIZE, 'train')
 				model.step(samples_batch, labels_batch, 'train')
 
 if __name__ == "__main__":
